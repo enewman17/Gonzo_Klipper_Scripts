612802599598d2a5e39d946bd14a6b66c64d8da3
Erin Newman <enewman17@users.noreply.github.com>
2025-03-16 01:13:50 -0400

    even more changes updates and fixes

---
+++ extras/max_mcu_size_override.py
@@ -0,0 +1,12 @@
+﻿from extras import neopixel
+
+class MaxMCUSizeOverride:
+    def __init__(self, config):
+        # Retrieve the MAX_MCU_SIZE value from the [max_mcu_size] section
+        max_mcu_size = config.getint('MAX_MCU_SIZE')
+        # Override the MAX_MCU_SIZE variable in the neopixel module
+        neopixel.MAX_MCU_SIZE = max_mcu_size
+
+def load_config(config):
+    # Klipper requires this function to instantiate the class
+    return MaxMCUSizeOverride(config)

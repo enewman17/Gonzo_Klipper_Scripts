ae43629aef9702bb70230125a8409c9a4ab00f4d
Erin Newman <enewman17@users.noreply.github.com>
2025-12-30 11:17:17 -0500

    Refactor configuration files and macros for improved clarity and error handling; update README for better usage instructions

--- README.md
+++ README.md
@@ -1,21 +1,19 @@
 # Gonzo_Klipper_Scripts
 
-Klipper Scripts for the Voron 2.4 and other printers running klipper
-BE WARNED IF YOU FIND THESES SCRIPTS THEY ARE UNDER DEVELOPMENT, SOME THINGS MAY NOT WORK 100% BECAUSE I HAVENT TESTED EVERYTHING THROUGHLY.
-BUT ALL IN ALL EVERYTHING IN THE DAY TO DAY STUFF WORKS.
+There are a lot of Klipper Macros available but nothing quite fit my usage or how I wanted things to work. 
+These macros were taylored for the 350 Voron 2.4 but should work with all printers running Klipper.
+
+# BE WARNED IF YOU FIND THESES SCRIPTS THEY ARE UNDER DEVELOPMENT, SOME THINGS MAY NOT WORK 100% BECAUSE I HAVENT TESTED EVERYTHING THROUGHLY.
 
 I have amassed a buch of macros and wrote even more to come up with a fairly comprehensive collections of controls for printers running klipper.
-There is no installer just yet, it's on my todo list... I started one but its not finised. Plus I need to iron out a lot of details for what to include. For now I have uploaded these for my own use and any power users who may come across these to use as they see fit. 
+There is no installer just yet, it's on my todo list... For now I have uploaded these for my own use and any power users who may come across these to use as they see fit. 
 
-A lot of macros for add-ons are heavliy intigrated making this quite a mess for mass use. If certain add ons like led-effect aren't installed you will get a lot of errors. I use an ERCF so Happy Hare is also pretty well intigrated into these macros. I added a lot of options and checks in the macro_variables to mitigate errors and bypass features if not in use or supported by one's printer. 
+A lot of macros for add-ons are heavliy intigrated making this quite a mess for mass use. If certain add ons like led-effect aren't installed you will get a lot of command not found errors. 
+I use an ERCF so Happy Hare is also pretty well intigrated into these macros. There are also a few changes to macros included with happy hare, lots of changes to blobifier and a set of maco helper for tuning.
+I added a lot of options and checks in the macro_variables to mitigate errors and bypass features if not in use or supported by one's printer. 
 
 
-
-Voron 2.4/Trident
-
-There are a lot of Klipper Macros available but nothing quite fit my usage or how I wanted things to work. The following macros were specifically writen for the 350 Voron 2.4 but should work with just about all printers running Klipper.
-
-Many thanks to
+###### Many thanks to:
 
 Justin Schuh: <https://github.com/jschuh/klipper-macros> A lot of code and ideas were borrowed from those macros.  
 
--- extras/test_speed_check.py
+++ extras/test_speed_check.py
@@ -1,18 +1,19 @@
-﻿# extras/test_speed_check.py
+﻿# to use this scrip in conjunction with test_speed macro place file in ~/klipper/klippy/extras/test_speed_check.py
+# Replace 'GET_POSITION' calls in test_speed macro with (1)'RECORD_START_POS' and (2)'COMPARE_POS' respectively
+
 class TestSpeedCheck:
     def __init__(self, config):
         self.printer = config.get_printer()
         self.gcode = self.printer.lookup_object('gcode')
         self.start_mcu_pos = {}
         
-        # Define descriptions for the G-Code terminal
+        # Descriptions for the macro commands
         self.cmd_RECORD_DESC = "Record MCU stepper positions and microsteps before test"
         self.cmd_COMPARE_DESC = "Compare MCU stepper positions to start and report Pass/Fail based on microsteps"
         
         self.register_commands()
 
     def register_commands(self):
-        # Registering with desc= as required by Klipper
         self.gcode.register_command('RECORD_START_POS', self.cmd_RECORD_START_POS, 
                                    desc=self.cmd_RECORD_DESC)
         self.gcode.register_command('COMPARE_POS', self.cmd_COMPARE_POS, 
@@ -29,10 +30,9 @@ class TestSpeedCheck:
             s_name = s.get_name()
             mcu_pos = s.get_mcu_position()
             
-            # Fetch microsteps from config, default to 16
-            microsteps = 16
-            if s_name in configfile.config:
-                microsteps = int(configfile.config[s_name].get('microsteps', 16))
+            microsteps = 16  # Default fallback
+            if s_name in configfile.settings:
+                microsteps = int(configfile.settings[s_name].microsteps.get('microsteps', 16))
             
             stepper_data[s_name] = {
                 'pos': mcu_pos,
@@ -41,45 +41,49 @@ class TestSpeedCheck:
         return stepper_data
 
     def cmd_RECORD_START_POS(self, gcmd):
-        self.start_mcu_pos = self.get_stepper_info()
-        gcmd.respond_info("TEST_SPEED: Initial positions recorded.")
+        try:
+            self.start_mcu_pos = self.get_stepper_info()
+            gcmd.respond_info("TEST_SPEED: Initial positions recorded.")
+        except Exception as e:
+            gcmd.respond_error("TEST_SPEED Error in RECORD_START_POS: %s" % (str(e),))
 
     def cmd_COMPARE_POS(self, gcmd):
         if not self.start_mcu_pos:
             gcmd.respond_info("TEST_SPEED: No starting positions found. Run RECORD_START_POS first.")
             return
 
-        end_mcu_data = self.get_stepper_info()
-        output = ["Speed Test Comparison Report:"]
-        overall_fail = False
-        
-        for s_name, start_data in self.start_mcu_pos.items():
-            start_val = start_data['pos']
-            u_steps = start_data['microsteps']
+        try:
+            end_mcu_data = self.get_stepper_info()
+            output = ["Speed Test Comparison Report:"]
+            overall_fail = False
             
-            if s_name in end_mcu_data:
-                end_val = end_mcu_data[s_name]['pos']
-                diff = abs(end_val - start_val)
+            for s_name, start_data in self.start_mcu_pos.items():
+                start_val = start_data['pos']
+                u_steps = start_data['microsteps']
                 
-                # Comparison: Pass if diff <= 1 full step (the microstep value)
-                status = "PASS"
-                if diff > u_steps:
-                    status = "FAIL"
-                    overall_fail = True
-                
-                # Detailed output as requested
-                output.append(
-                    f"{s_name}: Start={start_val} | End={end_val} | Diff={diff} | "
-                    f"Microsteps={u_steps} | Result: {status}"
-                )
+                if s_name in end_mcu_data:
+                    end_val = end_mcu_data[s_name]['pos']
+                    diff = abs(end_val - start_val)
+                    
+                    status = "PASS"
+                    if diff > u_steps:
+                        status = "FAIL"
+                        overall_fail = True
+                    
+                    output.append(
+                        f"{s_name}: Start={start_val} | End={end_val} | Diff={diff} | "
+                        f"Microsteps={u_steps} | Result: {status}"
+                    )
 
-        final_report = "\n".join(output)
-        if overall_fail:
-            final_report += "\n\n!!! WARNING: SKIPPING DETECTED !!!"
-        else:
-            final_report += "\n\nSUCCESS: No significant skipping detected."
-            
-        gcmd.respond_info(final_report)
+            final_report = "\n".join(output)
+            if overall_fail:
+                final_report += "\n\n!!! WARNING: SKIPPING DETECTED !!!"
+            else:
+                final_report += "\n\nSUCCESS: No significant skipping detected."
+                
+            gcmd.respond_info(final_report)
+        except Exception as e:
+            gcmd.respond_error("TEST_SPEED Error in COMPARE_POS: %s" % (str(e),))
 
 def load_config(config):
     return TestSpeedCheck(config)
